b620247a92f6b5cd96e6d4c03f5cf937036ede66
whaleen <joshuavaage@gmail.com>
2023-11-20 15:45:13 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
 export default async (req, res) => {
-  res.redirect(301, 'https://piratewires.us');
+
+  // Get the full path from the request
+  const path = req.url;
+
+  // Construct redirect URL with path
+  const redirectUrl = `https://piratewires.com${path}`;
+
+  res.redirect(301, redirectUrl);
 }
